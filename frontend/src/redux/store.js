559de653f6d8a6678228b935d5161e22f9622aed
Sundram Mishra <ram322222@gmail.com>
2026-05-28 20:03:35 +0530

    created redux store for frontend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import authReducer from './slices/authSlice'
+import cartReducer from './slices/cartSlice'
+import productReducer from './slices/productSlice'
+import orderReducer from './slices/orderSlice'
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  products: productReducer,
+  orders: orderReducer,
+})
+
+// Persist auth and cart across page refreshes
+const persistConfig = {
+  key: 'babafly',
+  storage,
+  whitelist: ['auth', 'cart'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
